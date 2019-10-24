9c66edb9c30d9a1dfebd58118f69b33985cac6fc
analogy47 <actweed47@gmail.com>
2019-10-24 17:14:25 -0400

    add calculator

--- calculator.kt
+++ calculator.kt
@@ -19,5 +19,62 @@
 
 /**
  * Constants
+ * use val
  */
 
+// string creation
+val ADD: String = "+"
+val SUBTRACT: String = "-"
+val MULTIPLY: String = "*"
+val DIVIDE: String = "/"
+
+val ERROR_MESSAGE = "Error has occured"
+
+
+val testOperand1 = 5.0
+val testOperand2 = 4.0
+
+// entry point of program
+fun main(args: Array<String>) {
+
+    // args is an Array or collection  of data (in this case, strings)
+    val operatorSymbol = args[0] 
+
+    start(operatorSymbol)
+}
+
+fun start(operatorSymbol: String ){
+    if (checkArguments(operatorSymbol)) {
+       displayResult(
+        evaluateBinomial(testOperand1, testOperand2, operatorSymbol)
+       )
+    } else {
+        displayResult(ERROR_MESSAGE)
+    }
+}
+
+// does the computation
+fun evaluateBinomial(opOne: Double, opTwo: Double, sym: String): String {
+    // 'fancy' conditional
+    when (sym) {
+            ADD -> return (opOne + opTwo).toString()
+            SUBTRACT -> return (opOne - opTwo).toString()
+            MULTIPLY -> return (opOne * opTwo).toString()
+            DIVIDE -> return (opOne / opTwo).toString()
+            else -> return ERROR_MESSAGE
+        }
+}
+
+// one-liner example
+fun displayResult(result: String) = System.out.println(result)
+
+// : => return
+fun checkArguments(operatorSymbol: String): Boolean {
+    when (operatorSymbol) { 
+        ADD -> return true
+        SUBTRACT -> return true
+        MULTIPLY -> return true
+        DIVIDE -> return true
+        else -> return false
+    }
+}

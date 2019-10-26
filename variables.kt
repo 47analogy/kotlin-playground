992d59dd58998d8b742ebcbada2a9b69f5d60986
analogy47 <actweed47@gmail.com>
2019-10-26 01:51:44 -0400

    update variables

--- variables.kt
+++ variables.kt
@@ -1,6 +1,11 @@
 //compiled time consts that never change
 const val ERROR_MESSAGE = "Error has occured"
 
+// The value for const val is determined at compile time, where as 
+// the value for val is determined during program execution, which means, 
+// val can be assigned by a function at run time.
+// val can be assigned a value from a function, but const val canno
+
 // use when variable visible is in different parts of the program
 internal const val ERROR_MESSAGE = "Error has occured"
 
@@ -25,3 +30,6 @@ fun renderFrame(moveX: Double, moveY: Double) {
     playerLocationX += moveX
     playerLocationY += moveY
 }
+
+val school = arrayOf("shark", "salmon", "minnow")
+println(java.util.Arrays.toString(school)) // need array utility to print contents instead of the address

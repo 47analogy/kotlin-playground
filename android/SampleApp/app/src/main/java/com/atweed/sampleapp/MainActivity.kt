fc3cc6808ea5b5bffc1d5a438118a99d9bc890fc
analogy47 <actweed47@gmail.com>
2019-10-27 00:09:25 -0400

    add buttons

--- android/SampleApp/app/src/main/java/com/atweed/sampleapp/MainActivity.kt
+++ android/SampleApp/app/src/main/java/com/atweed/sampleapp/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.atweed.sampleapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun turnOn(view: View) {
+        val openMessage = Toast.makeText(this, "App is on", Toast.LENGTH_SHORT);
+        openMessage.show()
+    }
+
+    fun displayMessage(view: View) {
+        val showMessageTextView = findViewById<TextView>(R.id.textView)
+        val newMessage = "Hello Earth"
+        showMessageTextView.text = newMessage
+    }
+
+    fun turnOff(view: View) {
+        val closeMessage = Intent(this, SecondActivity::class.java)
+        startActivity(closeMessage)
     }
 }
